6a37218d43785a1148137551e48212d8e1e966f4
Ramdoni <ramdoni@stalavista.com>
2024-01-29 01:40:43 +0700

    update

---
+++ database/migrations/2024_01_26_175356_add_field_running_cancel_cn_to_polis.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldRunningCancelCnToPolis extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('polis', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('polis', function (Blueprint $table) {
+            //
+        });
+    }
+}
