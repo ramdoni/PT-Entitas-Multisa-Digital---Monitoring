9679901aeb78a54937a121b2473843ea3d8505c5
Ramdoni <ramdoni@stalavista.com>
2024-02-05 00:08:33 +0700

    update

--- app/Http/Livewire/InvoiceIn/Index.php
+++ app/Http/Livewire/InvoiceIn/Index.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\InvoiceIn;
 
 use Livewire\Component;
+use App\Models\Invoice;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.invoice-in.index');
+        $data = Invoice::orderBy('id','DESC');
+
+        return view('livewire.invoice-in.index')->with(['data'=>$data->paginate(100)]);
     }
 }
